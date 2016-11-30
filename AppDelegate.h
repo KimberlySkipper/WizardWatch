7663721b4c26112d7834a530cd693b7322c1b7c4
Kimberly Skipper <kimmy.skipper@gmail.com>
2016-11-30 16:00:24 -0500

    basic wizard watch

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WizardWatch
+//
+//  Created by Marco Almeida on 11/30/16.
+//  Copyright © 2016 THE IRON YARD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
